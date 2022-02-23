2c6cb91c166290903dd06c66312c9886218ca8d9
Duc Luong <luongvanduc04101996@gmail.com>
2022-02-23 20:40:54 +0700

    lab 2.1 2.2 2.3

--- src/Lab2.java
+++ src/Lab2.java
@@ -5,6 +5,7 @@ public class Lab2 {
     static final double MIN_BMI_NORMAL = 18.5;
     static final double MAX_BMI_NORMAL = 24.9;
     static final double MAX_BMI_OVERWEIGHT = 29.9;
+
     public static void main(String[] args) {
         System.out.print("Please enter your height (m): ");
         Scanner scanner = new Scanner(System.in);
@@ -20,28 +21,25 @@ public class Lab2 {
     public static void GiveAdviceYourBody(double height, double weight) {
         DecimalFormat decimalFormat = new DecimalFormat("0.0");
         double bmiIndex = Double.parseDouble(decimalFormat.format(weight / (height * height)));
-        if (bmiIndex < MIN_BMI_NORMAL) {
-            double minIncreWeight = (MIN_BMI_NORMAL - bmiIndex) * (height * height);
-            double maxIncreWeight = (MAX_BMI_NORMAL - bmiIndex) * (height * height);
-            System.out.printf("====> Underweight!!!!\n====> You should increase %.1f kg to %.1f kg to have normal weight\n", minIncreWeight, maxIncreWeight);
-        } else if (bmiIndex <= MAX_BMI_NORMAL){
+        if (MIN_BMI_NORMAL <= bmiIndex && bmiIndex <= MAX_BMI_NORMAL) {
             System.out.println("====> Normal weight!!!\n====> Your body is so beautiful");
-        } else {
-            double maxDecreWeight = (bmiIndex - MIN_BMI_NORMAL) * (height * height);
-            double minDecreWeight = (bmiIndex - MAX_BMI_NORMAL) * (height * height);
-            if (bmiIndex <= MAX_BMI_OVERWEIGHT) {
-                System.out.println("====> Overweight!!!");
-            } else {
-                System.out.println("====> Obesity!!!");
-            }
-            System.out.printf("====> You should decrease %.1f kg to %.1f kg to have normal weight\n", minDecreWeight, maxDecreWeight);
+            return;
         }
+        double minChangeWeight = Math.abs((bmiIndex - MIN_BMI_NORMAL)) * (height * height);
+        double maxChangeWeight = Math.abs((bmiIndex - MAX_BMI_NORMAL)) * (height * height);
+        if (bmiIndex < MIN_BMI_NORMAL) {
+            System.out.printf("====> Underweight!!!!\n====> You should increase %.1f kg to %.1f kg to have normal weight\n", minChangeWeight, maxChangeWeight);
+            return;
+        } else if (bmiIndex <= MAX_BMI_OVERWEIGHT) {
+            System.out.println("====> Overweight!!!");
+        } else {
+            System.out.println("====> Obesity!!!");
+        }
+        System.out.printf("====> You should decrease %.1f kg to %.1f kg to have normal weight\n", maxChangeWeight, minChangeWeight);
     }
 
-    public static String OddOrNot(int number){
-        if ((number % 2) == 0) {
-            return "This is even number";
-        }
-        return "This is odd number";
+    public static String OddOrNot(int number) {
+        String result = (number % 2) != 0 ? "This is odd number" : "This is even number";
+        return result;
     }
 }
